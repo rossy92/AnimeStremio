73edcdb0200e8e2d70bf1cfdb53bf1f0ff070169
rossy92 <60351669+rossy92@users.noreply.github.com>
2026-01-12 13:09:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const { serveHTTP } = require("stremio-addon-sdk");
 const addonInterface = require("./addon");
 
-// Koyeb imposta la porta automaticamente, ma noi usiamo 8000 come base
 const port = process.env.PORT || 8000;
 
-// Forza l'ascolto su 0.0.0.0 (fondamentale per i servizi cloud)
-serveHTTP(addonInterface, { 
-    port: port, 
-    hostname: "0.0.0.0" 
-});
+serveHTTP(addonInterface, { port: port });
 
-console.log(`Server pronto sulla porta ${port}`);
+console.log(`Addon pronto sulla porta ${port}`);
