d56a2df95aeaadd2b3745b9ff61da7e50a6edb96
rossy92 <60351669+rossy92@users.noreply.github.com>
2026-01-12 10:44:17 +0100

    Set up HTTP server for Stremio addon

--- server.js
+++ server.js
@@ -1,1 +1,4 @@
+const { serveHTTP } = require("stremio-addon-sdk");
+const addonInterface = require("./addon"); // Il file che abbiamo visto prima
 
+serveHTTP(addonInterface, { port: process.env.PORT || 7000 });
