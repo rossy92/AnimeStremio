f55e5e52b101b0e1117fdbc6c8e67cf8879682a1
rossy92 <60351669+rossy92@users.noreply.github.com>
2026-01-12 12:11:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const addonInterface = require("./addon");
 
 // Questa riga permette a servizi come Koyeb o Render di assegnare
 // automaticamente una porta. Se lo provi in locale, userà la 7000.
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 8000;
 
 serveHTTP(addonInterface, { port: port });
 
