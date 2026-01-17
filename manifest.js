bfd6221ea518866a8314efdf5159a6b0b26ee811
rossy92 <60351669+rossy92@users.noreply.github.com>
2026-01-17 17:07:02 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,10 +1,14 @@
-module.exports = {
-  id: "animestremio",
-  version: "1.0.0",
-  name: "AnimeStremio",
-  description: "Stremio addon for English dubbed anime",
-  types: ["series"],
-  catalogs: [],           // array vuoto perché stream-only
-  resources: ["stream"],
-  idPrefixes: ["anilist:"]
+const { addonBuilder } = require("stremio-addon-sdk");
+
+const manifest = {
+    "id": "org.animestremio.rossy",
+    "version": "1.0.0",
+    "name": "AnimeStremio",
+    "resources": ["stream"],
+    "types": ["series", "movie"],
+    "idPrefixes": ["tt", "kitsu"] // tt per IMDb, kitsu per gli anime
 };
+
+const builder = new addonBuilder(manifest);
+
+module.exports = builder;
