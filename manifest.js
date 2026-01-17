57f4bc438a6c5623f93d92960acebc251c5de232
rossy92 <60351669+rossy92@users.noreply.github.com>
2026-01-17 15:18:53 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,1 +1,9 @@
-
+module.exports = {
+  id: "org.animestremio.engdub",
+  version: "0.1.0",
+  name: "Anime ENG Dub",
+  description: "Watch English dubbed anime from external providers",
+  resources: ["stream"],
+  types: ["series"],
+  idPrefixes: ["anilist"]
+};
