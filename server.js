a7e73afed6eb709c6178d59551c943b25d98af5f
rossy92 <60351669+rossy92@users.noreply.github.com>
2026-01-17 16:36:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,26 +3,26 @@ const manifest = require("./manifest");
 
 const builder = new addonBuilder(manifest);
 
-// STREAM HANDLER
+// STREAM HANDLER minimo
 const { getStreamsForEpisode: getPaheStreams } = require("./providers/animepahe");
 const { getStreamsForEpisode: getKaiStreams } = require("./providers/animekai");
 
 builder.defineStreamHandler(async ({ type, id }) => {
-  try {
-    if (type !== "series") return { streams: [] };
+  if (type !== "series") return { streams: [] };
 
+  try {
     const parts = id.split(":");
     if (parts.length !== 3) return { streams: [] };
 
     const anilistId = parts[1];
     const episode = parts[2];
 
+    // solo array vuoti per test
     let streams = await getPaheStreams(anilistId, episode);
     if (streams.length > 0) return { streams };
 
     streams = await getKaiStreams(anilistId, episode);
     return { streams };
-
   } catch (err) {
     console.error("Stream error:", err);
     return { streams: [] };
