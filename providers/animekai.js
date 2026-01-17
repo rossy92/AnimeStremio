b2d30fbcf073f5f03b6b94ed944305b7a35d1554
rossy92 <60351669+rossy92@users.noreply.github.com>
2026-01-17 16:38:04 +0100

    Update animekai.js

--- providers/animekai.js
+++ providers/animekai.js
@@ -1,27 +1,6 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
-
 async function getStreamsForEpisode(anilistId, episode) {
-  try {
-    // AnimeKai URL base
-    const url = `https://animekai.com/anime/${anilistId}/episode-${episode}`;
+  // test: array vuoto
+  return [];
+}
 
-    // Fetch pagina
-    const res = await axios.get(url);
-    const $ = cheerio.load(res.data);
-
-    // Estrai iframe / video source
-    const iframeSrc = $("iframe").attr("src");
-    if (!iframeSrc) return [];
-
-    // Mappa stream
-    return [
-      {
-        title: `Episode ${episode}`,
-        url: iframeSrc,
-        quality: "HD",
-        subtitles: []
-      }
-    ];
-
-  } catch (err) {
+module.exports = { getStreamsForEpisode };
