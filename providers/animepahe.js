d4eaaac4dbdbc7fb1b8f7f70032561511621d2d9
rossy92 <60351669+rossy92@users.noreply.github.com>
2026-01-17 16:37:40 +0100

    Update animepahe.js

--- providers/animepahe.js
+++ providers/animepahe.js
@@ -1,17 +1,6 @@
-const axios = require("axios");
-
 async function getStreamsForEpisode(anilistId, episode) {
-  try {
-    // AnimePahe API unofficial
-    // Cerca anime per AniList ID e episodio
-    const searchUrl = `https://animepahe.com/api?m=release&id=${anilistId}&episode=${episode}`;
-    const res = await axios.get(searchUrl);
-    const data = res.data;
+  // test: array vuoto
+  return [];
+}
 
-    if (!data || !data.data || data.data.length === 0) {
-      return [];
-    }
-
-    // Mappa gli stream
-    const streams = data.data.map(item => ({
-      title: item.title,
+module.exports = { getStreamsForEpisode };
